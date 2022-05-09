64d80176c54b7a30a656b6e05e4d8fb2270b6c12
Ankit Kapoor <ankitkapoor8437@gmail.com>
2022-05-09 12:15:13 +0530

    Completed the 14 day of algorithem study plan

--- test.cpp
+++ test.cpp
@@ -1,49 +1,10 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
+
 using namespace std;
 
 int main()
 {
-    vector<int> arr;
 
-    arr.push_back(9);
-    arr.push_back(27);
-    arr.push_back(2);
-    arr.push_back(13);
-    arr.push_back(25);
-    arr.push_back(2);
-
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout<< arr[i] << " ";
-    }
-    cout<< endl;
-    
-    // sort(arr.begin(), arr.end());
-
-
-    for (int i = 0; i < arr.size()-1;)
-    {
-        if (arr[i]>arr[i+1])
-        {
-            swap(arr[i], arr[i+1]);
-            i = -1;
-        }
-        i++;
-    }
-
-        
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout<< arr[i] << " ";
-    }
-    cout<< endl;
-
-    int i = arr.size()-1;
-
-    cout<< arr[arr.size()-2];
-    
     
     
 
