fd107183c0a0be7f742aa793d393f51c828772a8
Ankit Kapoor <ankitkapoor8437@gmail.com>
2022-05-12 13:27:30 +0530

    implementaion of hashmap, bucket array, complexity analysis

---
+++ TOPICS/HASHMAPS/bucket_array.cpp
@@ -0,0 +1,20 @@
+// discussed about the theory part
+
+
+//Hashcode
+//compression funtion
+
+//Collisions
+
+    //Collision Handling
+    //1. Open Hashing (Seperate Chaining)
+    //2. Cloased Addressing
+        //linear probing
+        //quadratic probing
+
+//Complexity analysis
+    //n->no. of entries in map.
+    //b->no. of boxes available
+
+    // (n/b) < 0.7 is the load factor
+        //Rehasgin
